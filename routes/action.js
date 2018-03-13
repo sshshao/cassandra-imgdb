43d972315fb78fdba3d6b51aefb36679751006ea
S Shao <sshao14@gmail.com>
2018-03-13 03:31:08 -0400

    endpoints set

--- routes/action.js
+++ routes/action.js
@@ -1,11 +1,39 @@
 const cassandra = require('cassandra-driver');
-const client = new cassandra.Client({ contactPoints: ['h1', 'h2'], keyspace: 'hw4' });
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1:9042'], keyspace: 'hw4' });
 
 
 exports.deposit = function(req, res) {
-
+    if(req.body.filename != null && req.body.contents != null) {
+        var query = 'INSERT INTO imgs (filename, contents) VALUES (?, ?)';
+        var params = [req.body.filename, req.body.contents];
+        client.execute(query, params, { prepare: true }, function(err, result) {
+            console.log(result);
+        });
+    }
 }
 
 exports.retrieve = function(req, res) {
-
+    if(req.body.filename != null) {
+        var query = 'SELECT filename, contents FROM imgs WHERE filename = ?';
+        client.execute(query, [req.body.filename], function(err, result) {
+            assert.ifError(err);
+            if(result.rows == 1) {
+                console.log('[*]filename %s', result.rows[0].filename);
+                console.log('[*]contents %s', result.rows[0].contents);
+                res.send({
+                    'status': 'OK'
+                });
+            }
+            else {
+                res.send({
+                    'status': 'ERROR'
+                });
+            }
+        });
+    }
+    else {
+        res.send({
+            'status': 'ERROR'
+        });
+    }
 }
